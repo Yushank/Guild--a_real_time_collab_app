0b02daef6792393fd22a9881d47623f7a1424782
Yushank <yushank2001@gmail.com>
2025-04-13 11:44:22 +0530

    next config update

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   experimental: {
-    serverComponentsExternalPackages: ["@next-auth/prisma-adapter"],
-  }
+    serverComponentsExternalPackages: [
+      "@next-auth/prisma-adapter",
+      "@prisma/client"
+    ],
+  },
 };
 
 export default nextConfig;
